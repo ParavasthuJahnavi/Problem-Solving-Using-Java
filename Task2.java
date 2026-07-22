e44d7faec71600df9ffdabdfbeec3337ed5cfc3a
Jahnavi <p.jahnavi1276@gmail.com>
2026-07-22 21:51:22 +0530

    First Commit

---
+++ Task2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int[] arr = {-7, -5, 5, 10, 0, 3, 20, 25, 12};
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        if (index >= 0 && index < arr.length) {
+            System.out.println("Element at index " + index + " is: " + arr[index]);
+        } else {
+            System.out.println("Invalid index");
+        }
+
+        sc.close();
+    }
+}
